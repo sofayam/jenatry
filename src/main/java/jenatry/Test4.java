43ec4519c867d201aaf8a6037d714976bdd8405c
Mark Andrew <mark.andrew@de.bosch.com>
2017-08-10 13:59:26 +0200

    Shifted crashing loop to Test5

--- src/main/java/jenatry/Test4.java
+++ src/main/java/jenatry/Test4.java
@@ -7,9 +7,11 @@ import java.io.InputStream;
 
 import java.util.Iterator;
 
+import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.OntResource;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -39,7 +41,7 @@ public class Test4 {
 		
 		
 		if (validity.isValid()) {
-		    System.out.println("OK");
+		    System.out.println("OK model is valid");
 		} else {
 		    System.out.println("Conflicts");
 		    for (Iterator<Report> i = validity.getReports(); i.hasNext(); ) {
@@ -47,59 +49,39 @@ public class Test4 {
 		    } 
 		}
 		
-		/* Now find newly generated facts (entailed statements) */ 
+		InputStream s2 = Test2.class.getResourceAsStream("/PulseMoodInferOnto.owl");
 		
-		/* Where are the Due Reminders? */
-		
-		OntModel ontModel = ModelFactory.createOntologyModel();
-		
-		/* partially stolen from https://stackoverflow.com/questions/3024273/inferring-using-jena */
-		
-		InputStream s2 = Test2.class.getResourceAsStream("/PulseMoodInferOnto.rdfxml");
-		ontModel.read(s2,"RDF/XML");
-		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-		reasoner = reasoner.bindSchema(ontModel);
-		OntModelSpec ontModelSpec = OntModelSpec.OWL_DL_MEM;
-		ontModelSpec.setReasoner(reasoner);
-		OntModel model = ModelFactory.createOntologyModel(ontModelSpec, ontModel);
-		
-		ExtendedIterator<OntClass> it = model.listClasses();
-		
-		 while (it.hasNext()) {
-			System.out.println(it.next().toString());
-		}
-		
-		//String dueReminder = "http://www.semanticweb.org/antonioskaratzoglou/ontologies/2017/7/mood_person_onto#DueReminder";
-		
-		 System.out.println("Listed all classes");
-		printSubclasses(model, "DueReminder");
-		
-		//OntClass dueReminderClass = model.getOntClass(dueReminder);
-/*		
-		ExtendedIterator<OntClass> sci = dueReminderClass.listSubClasses();
-		
-		System.out.println("************ subclasses of " + dueReminder);
-		
-		while (sci.hasNext()) {
-			OntClass found = sci.next();
-			System.out.println(found.toString());
-		}*/
-	}
+	    OntModel model = ModelFactory.createOntologyModel();
 
-	public static void printSubclasses(OntModel model, String className) {
-		String fullName = "http://www.semanticweb.org/antonioskaratzoglou/ontologies/2017/7/mood_person_onto#" + className;
-		OntClass ontClass = model.getOntClass(fullName);
+	    model.read(s2,null,"TTL");
 		
-		ExtendedIterator<OntClass> sci = ontClass.listSubClasses();
+	    ExtendedIterator<OntClass> classes = model.listClasses();
+
+	    while (classes.hasNext())
+
+	    {
+
+	      OntClass thisClass = (OntClass) classes.next();
+
+	      System.out.println("Found class: " + thisClass.toString());
+
+	      ExtendedIterator<? extends OntResource> instances = thisClass.listInstances();
+
+	      while (instances.hasNext())
+
+	      {
+
+	        Individual thisInstance = (Individual) instances.next();
+
+	        System.out.println("  Found instance: " + thisInstance.toString());
+
+	      }
+
+	    }
+
+
 		
-		System.out.println("************ subclasses of " + className);
-		
-		while (sci.hasNext()) {
-			OntClass found = sci.next();
-			System.out.println(found.toString());
-		}
 	}
-	
 	public static void main(String[] args) {
 		doit();
 	}
---
+++ src/main/java/jenatry/Test5.java
@@ -0,0 +1,91 @@
+package jenatry;
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+import java.util.Iterator;
+
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.reasoner.ValidityReport.Report;
+import org.apache.jena.util.iterator.ExtendedIterator;
+
+
+
+public class Test5 {
+
+	static void doit() {
+				
+		/* Now find newly generated facts (entailed statements) */ 
+		
+		/* Where are the Due Reminders? */
+		
+		OntModel ontModel = ModelFactory.createOntologyModel();
+		
+		/* partially stolen from https://stackoverflow.com/questions/3024273/inferring-using-jena */
+		
+		InputStream s2 = Test2.class.getResourceAsStream("/PulseMoodInferOnto.rdfxml");
+		ontModel.read(s2,"RDF/XML");
+		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+		reasoner = reasoner.bindSchema(ontModel);
+		OntModelSpec ontModelSpec = OntModelSpec.OWL_DL_MEM_RULE_INF;
+		ontModelSpec.setReasoner(reasoner);
+		OntModel model = ModelFactory.createOntologyModel(ontModelSpec, ontModel);
+		
+		ExtendedIterator<OntClass> it = model.listClasses();
+		
+		// while (it.hasNext()) {
+		//	System.out.println(it.next().toString());
+		//}
+		
+		//String dueReminder = "http://www.semanticweb.org/antonioskaratzoglou/ontologies/2017/7/mood_person_onto#DueReminder";
+		
+		//System.out.println("Listed all classes");
+		
+		
+		//printSubclasses(model, "DueReminder");
+		printSubclasses(model, "Reminder");
+		
+		//OntClass dueReminderClass = model.getOntClass(dueReminder);
+/*		
+		ExtendedIterator<OntClass> sci = dueReminderClass.listSubClasses();
+		
+		System.out.println("************ subclasses of " + dueReminder);
+		
+		while (sci.hasNext()) {
+			OntClass found = sci.next();
+			System.out.println(found.toString());
+		}*/
+	}
+
+	public static void printSubclasses(OntModel model, String className) {
+		
+		String fullName = "http://www.semanticweb.org/antonioskaratzoglou/ontologies/2017/7/mood_person_onto#" + className;
+		OntClass ontClass = model.getOntClass(fullName);
+		
+		ExtendedIterator<OntClass> sci = ontClass.listSubClasses();
+		
+		System.out.println("************ subclasses of " + className);
+		
+		while (sci.hasNext()) {
+			OntClass found = sci.next();
+			System.out.println(found.toString());
+		}
+	}
+	
+	public static void main(String[] args) {
+		doit();
+	}
+
+
+}
+
