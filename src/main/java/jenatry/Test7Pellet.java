c8b47488bff9ef0f83c9763fe1a4cd1bc0c3fe57
Mark Andrew <mark.andrew@de.bosch.com>
2017-08-11 11:11:37 +0200

    Attempt with Pellet

---
+++ src/main/java/jenatry/Test7Pellet.java
@@ -0,0 +1,16 @@
+package jenatry;
+
+public class Test7Pellet {
+
+	static void doit() {
+				
+		System.out.println("Pellet is a mystery to me");
+	}
+	
+	public static void main(String[] args) {
+		doit();
+	}
+
+
+}
+
