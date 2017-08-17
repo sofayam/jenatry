0676ac64143047f1b8016c2f330d75fd8916b172
Mark Andrew <mark.andrew@de.bosch.com>
2017-08-17 16:13:39 +0200

    Minimal query of due reminders

--- src/main/java/jenatry/ReminderBetter.java
+++ src/main/java/jenatry/ReminderBetter.java
@@ -10,22 +10,23 @@ public class ReminderBetter {
 	public static void doit() {
 		OntoUtils onto = new OntoUtils(
 				"http://www.semanticweb.org/anm2fr/ontologies/2017/7/reminder#", 
-				"C:\\work\\repos\\ontologies\\Reminder\\ReminderOntoMark.owl");
+				"C:\\work\\repos\\ontologies\\Reminder\\ReminderOntoMarkComplex.owl");
 		
-		System.out.println("Getting named instances of Person");
+		System.out.println("Getting named instances of DueReminder");
 		
-		NodeSet<OWLNamedIndividual> peeps = onto.getNamedInstances("Person");
-		for (OWLNamedIndividual i : peeps.getFlattened()) {
+		NodeSet<OWLNamedIndividual> rems = onto.getNamedInstances("DueReminder");
+		for (OWLNamedIndividual i : rems.getFlattened()) {
 			System.out.println(i.getIRI().toString());
 		}
 		
+		/*
 		System.out.println("Getting a data property");
 		
 		OWLLiteral ol = onto.getDataProperty("mark", "hasGoodMood");
 		
 		System.out.println(ol.toString());
 		
-		
+		*/
 		
 	}
 	
